5595ba49180b12d0514f890f5d8186d3f7ffe3f4
DateDay098010 <b11015055@gapps.ntust.edu.tw>
2023-04-15 22:09:18 +0800

    add loading screen function

--- HW2/Assets/Scripts/MainManager.cs
+++ HW2/Assets/Scripts/MainManager.cs
@@ -1,16 +1,27 @@
+using System.Collections;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class MainManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject _loadingScreen;
+    [SerializeField] private Image _loadingBarFill;
     void Start()
     {
         Application.targetFrameRate = 60;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void LoadScene (int sceneId) 
     {
-        
+        StartCoroutine(LoadSceneAsync(sceneId));
+    }
+    IEnumerator LoadSceneAsync(int sceneId) 
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+        _loadingScreen.SetActive(true);
+        while(!operation.isDone)
+        {
+            _loadingBarFill.fillAmount = Mathf.Clamp01(operation.progress / 0.9f);
+            yield return null;
+        }
     }
 }
