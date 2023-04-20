202dcd0f42f7ce1b51b38a3d96c80e6038e8f548
DateDay098010 <b11015055@gapps.ntust.edu.tw>
2023-04-20 22:05:24 +0800

    Add Enemy Attack func (not complete)

--- HW2/Assets/Scripts/EnemyAIController.cs
+++ HW2/Assets/Scripts/EnemyAIController.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 // [RequireComponent(typeof(BoxCollider2D))]
 public class EnemyAIController : MonoBehaviour
 {
+    [SerializeField] GameObject _player;
+    // To store waypoints
     [SerializeField] private List<Transform> _points;
+    // Which point to move
     [SerializeField] private int _nextID = 1;
+    // To update _nextID with increment or decrement
     [SerializeField] private int _IDChangeValue = 1;
+    // The speed of Moving to the waypoint
     [SerializeField] private float _speed = 2f;
+
+    [SerializeField] private float _attckDistance = 2f;
+    [SerializeField] private float _timeBetweenAttack = 3f;
     private void Reset()
     {
         Init();
     }
     private void Init()
     {
+        _player = GameObject.FindWithTag("Player");
+
         // GetComponent<BoxCollider2D>().isTrigger = true;
         
         GameObject root = new GameObject(name + " Root");
@@ -37,9 +47,20 @@ public class EnemyAIController : MonoBehaviour
     }
     private void Update()
     {
-        MoveToNextPoint();
+        if(Vector2.Distance(transform.position, _player.transform.position) < _attckDistance)
+        {
+            // check if facing player
+            bool faceRight =  _player.transform.position.x >= transform.position.x && transform.localScale.x > 0;
+            bool faceLeft =  _player.transform.position.x <= transform.position.x && transform.localScale.x < 0;
+            if(faceLeft || faceRight)
+            {
+                Attack();
+            }
+        }
+        else
+            Patrol();
     }
-    private void MoveToNextPoint()
+    private void Patrol()
     {
         Transform goalPoint = _points[_nextID];
 
@@ -53,7 +74,7 @@ public class EnemyAIController : MonoBehaviour
         transform.position = Vector2.MoveTowards(transform.position, goalPoint.position, _speed * Time.deltaTime);
 
         //set next point
-        if(Vector2.Distance(transform.position, goalPoint.position) < 1f)
+        if(Vector2.Distance(transform.position, goalPoint.position) < 0.3f)
         {
             // move backward
             if(_nextID == _points.Count - 1)
@@ -64,5 +85,16 @@ public class EnemyAIController : MonoBehaviour
             _nextID += _IDChangeValue;
         }
                 
+    }
+    private void Attack()
+    {
+        //TODO: attck player
+
+        // time between attcking
+        float timer = 0f;
+        while(timer < _timeBetweenAttack)
+        {
+            timer += Time.deltaTime;
+        }
     }
 }
